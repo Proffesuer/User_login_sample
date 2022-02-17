194d698372216cae5af4621cd3a090a2e25757f7
Proffesuer <levischisira0@gmail.com>
2022-02-17 13:33:34 +0300

    created my log in form

--- login.php
+++ login.php
@@ -10,21 +10,44 @@
 <body>
 
 
-
+<center>
 <div class="container">
-    <img src="picture/catholic.jpg"width="40%%">
+    
+    <img src="picture/catholic.png"width="100px">
+    <br><br>
+<lable>
 
-<form>
+USER LOGIN
+</lable>
+<BR><BR>
+<HR>
 
+<form class="back">
 
+<div class="form_input" align="center">
+    <lable>
+        Username:
+</lable>
+<br>
+
+<input type="text" name="username"placeholder="Enter username"><br>
+<lable>
+    Password:
+</lable><br>
+
+<input type="password" name="password"placeholder="Enter Password"><br>
+
+</div><br>
+<input type="submit"name="LOGIN"class="Button">
 
 
 
 </form>
+<br><br>
 </div>
 
 
 
-    
+</center>
 </body>
 </html>
