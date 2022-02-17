9a7158ae8d0271bc14c64c9e06caf41b545fca65
Proffesuer <levischisira0@gmail.com>
2022-02-17 18:40:32 +0300

    Added my php scripts to query data from and to the database

--- login.php
+++ login.php
@@ -1,3 +1,44 @@
+<?php
+
+
+$host="localhost";
+$user="root";
+$password="";
+$db="demo";
+
+  mysql_connect($host,$user,$password);
+   $mysql_select_db($db);
+if(isset($_POST['"username"'])){
+
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    $sql="select*from loginform where User='".$uname."'AND Pass='".$password."'limit 1";
+
+    $result=mysql_query($sql);
+    
+    if(mysql_num_rows($result)==1)
+    {
+        echo "You have successfully log in!!";
+        exit();
+    }else
+    {
+        echo "Entered invalid values,,,,Password or username";
+        exit();
+
+    }
+}
+
+
+
+
+
+?>
+
+
+/*my html code */
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -22,7 +63,7 @@ USER LOGIN
 <BR><BR>
 <HR>
 
-<form class="back">
+<form class="back" method="POST"action="#">
 
 <div class="form_input" align="center">
     <lable>
@@ -30,15 +71,15 @@ USER LOGIN
 </lable>
 <br>
 
-<input type="text" name="username"placeholder="Enter username"><br>
+<input type="text" name="username"placeholder="Enter username"required><br>
 <lable>
     Password:
 </lable><br>
 
-<input type="password" name="password"placeholder="Enter Password"><br>
+<input type="password" name="password"placeholder="Enter Password"required><br>
 
 </div><br>
-<input type="submit"name="LOGIN"class="Button">
+<input type="SUBMIT"name="LOGIN"class="Button1">
 
 
 
