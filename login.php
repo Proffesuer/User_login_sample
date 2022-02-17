1b3cf41d2b00339133551f8dfc727b84328ac269
Proffesuer <levischisira0@gmail.com>
2022-02-17 12:44:54 +0300

    Added my login.php,,,,loginstyle.css,,,and pictures to used in this system

---
+++ login.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user log in </title>
+    <link rel="stylesheet" href="loginstyle.css"type="text/css">
+
+</head>
+<body>
+
+
+
+<div class="container">
+    <img src="picture/catholic.jpg"width="40%%">
+
+<form>
+
+
+
+
+
+</form>
+</div>
+
+
+
+    
+</body>
+</html>
