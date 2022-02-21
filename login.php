0d64784a9378af141f12f443fe05525b06ac5873
bootrae <shadivasam@gmail.com>
2022-02-21 11:54:42 +0300

    removed two php lines

--- login.php
+++ login.php
@@ -6,8 +6,7 @@ $user="root";
 $password="";
 $db="demo";
 
-  mysql_connect($host,$user,$password);
-   $mysql_select_db($db);
+
 if(isset($_POST['"username"'])){
 
     $uname=$_POST['username'];
@@ -15,7 +14,7 @@ if(isset($_POST['"username"'])){
     $sql="select*from loginform where User='".$uname."'AND Pass='".$password."'limit 1";
 
     $result=mysql_query($sql);
-    
+
     if(mysql_num_rows($result)==1)
     {
         echo "You have successfully log in!!";
@@ -53,7 +52,7 @@ if(isset($_POST['"username"'])){
 
 <center>
 <div class="container">
-    
+
     <img src="picture/catholic.png"width="100px">
     <br><br>
 <lable>
